b3a310ab0acc95c827a10389b48c11a862d8a761
Petr Václavek <petr@vaclavek.cloud>
2025-12-22 14:23:30 +0100

    feat: x-session-token support (#2)
    * feat: x-session-token support
    
    * chore: bump php version in CI/CD

--- src/Guard.php
+++ src/Guard.php
@@ -15,13 +15,15 @@ class Guard
 {
     protected const KRATOS_SESSION_COOKIE = 'ory_kratos_session';
 
+    protected const KRATOS_SESSION_HEADER = 'X-Session-Token';
+
     public function __construct(protected FrontendApi $api)
     {
     }
 
     public function __invoke(Request $request, UserProvider $provider): ?Authenticatable
     {
-        $session = $this->getKratosSession($request->cookie(static::KRATOS_SESSION_COOKIE));
+        $session = $this->getKratosSession($request->cookie(static::KRATOS_SESSION_COOKIE), $request->header(static::KRATOS_SESSION_HEADER));
         if (! ($identity = $session?->getIdentity())) {
             return null;
         }
@@ -29,19 +31,27 @@ class Guard
         return $provider->retrieveById($identity);
     }
 
-    protected function getKratosSession(?string $cookie): ?Session
+    protected function getKratosSession(?string $cookie, ?string $session_token): ?Session
     {
-        if (! $cookie) {
+        if (! $cookie && ! $session_token) {
             return null;
         }
 
-        try {
-            $session = $this->api->toSession(cookie: static::KRATOS_SESSION_COOKIE."=$cookie");
-        } catch (ApiException) {
-            return null;
+        if ($cookie) {
+            try {
+                $session = $this->api->toSession(cookie: static::KRATOS_SESSION_COOKIE."=$cookie");
+            } catch (ApiException) {
+                return null;
+            }
         }
 
-        return $session instanceof Session
-            ? $session : null;
+        if ($session_token) {
+            try {
+                $session = $this->api->toSession(xSessionToken: $session_token);
+            } catch (ApiException) {
+                return null;
+            }
+        }
+        return $session instanceof Session ? $session : null;
     }
 }
